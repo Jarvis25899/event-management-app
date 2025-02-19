a1aed52b49d9754005481477b7f27657cc064c43
Jainil Patel <jainil.patel@peoplehum.com>
2025-02-19 22:52:21 +0530

    Added required components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'event-management-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { EventListComponent } from './components/event-list/event-list.component';
+import { EventDetailsComponent } from './components/event-details/event-details.component';
+import { EditEventComponent } from './components/edit-event/edit-event.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'events',
+    pathMatch: 'full',
+  },
+  {
+    path: 'events',
+    children: [
+      {
+        path: '',
+        component: EventListComponent,
+      },
+      {
+        path: 'create',
+        component: EditEventComponent,
+      },
+      {
+        path: 'view/:id',
+        component: EventDetailsComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: EditEventComponent,
+      },
+    ],
+  },
+];
